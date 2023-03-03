f76447d2da6a2cfe90ce37baf960004cb7d52177
TheFabulousP <10421100@student.vgu.edu.vn>
2023-03-03 21:34:37 +0700

    health bar for the tablet

--- src/game.lua
+++ src/game.lua
@@ -3,15 +3,18 @@ local Tablet = require 'src/tablets/tablet'
 local Game = {}
 
 function Game:enter()
-  self.tablet = Tablet()
+  self.tablet1 = Tablet(100,100,"blue","archer", 10, 1, 978)
+  -- self.tablet2 = Tablet(100,100,"blue", 10, 1, 278)
 end
 
 function Game:update(dt)
-  self.tablet:update(dt)
+  self.tablet1:update(dt)
+  -- self.tablet2:update(dt)
 end
 
 function Game:draw()
-  self.tablet:draw()
+  self.tablet1:draw()
+  -- self.tablet2:draw()
 end
 
 return Game
--- src/tablets/tablet.lua
+++ src/tablets/tablet.lua
@@ -1,11 +1,19 @@
 local Tablet = Class('Tablet')
 
-function Tablet:initialize(team, atk, maxhp)
-  self.team = team
+healthBarWidth = 40
+healthBarHeight = 7
+
+function Tablet:initialize(x,y,team,type, atk, atkspd, maxhp)
+  self.x, self.y = x,y
+  self.team = team or 1
   self.atk = atk or 1
-  self.maxhp, self.hp = maxhp
+  self.atkspd = atkspd or 1
+  self.maxhp, self.hp = maxhp, maxhp
   self.isDead = false
+
+  self.hp = 0
   -- love.graphics.newImage
+
 end
 
 function Tablet:receiveDamage(damage)
@@ -21,12 +29,34 @@ function Tablet:healHP(heal)
 end
 
 function Tablet:update(dt)
-
+  if self.hp == 0 then
+    isDead = true
+  end
 end
 
 function Tablet:draw()
   if not isDead then
-    love.graphics.circle('line',0,0,10)
+    --Draw Health Bar
+    if (self.maxhp > 500) then
+      local healthBarWidth = 1.5*healthBarWidth
+      if (self.hp > 0.5*self.maxhp) then
+        love.graphics.setColor(0,200,0)
+      elseif (self.hp > 0.3*self.maxhp) then
+        love.graphics.setColor(255,255,0)
+      else
+        love.graphics.setColor(255,0,0)
+      end
+      love.graphics.rectangle('fill',self.x-healthBarWidth/2,self.y-25,healthBarWidth*self.hp/self.maxhp,healthBarHeight)
+      love.graphics.setColor(255,255,255)
+      love.graphics.rectangle('line',self.x-healthBarWidth/2,self.y-25,healthBarWidth,healthBarHeight)
+      segment = self.maxhp/100
+      for i = 0, math.ceil(segment)-1 do
+        if i < (math.ceil(segment)-1) then
+          love.graphics.rectangle('line',self.x-healthBarWidth/2+i*healthBarWidth/segment,self.y-25,healthBarWidth/segment,healthBarHeight)
+        end
+      end
+    end
+    love.graphics.circle('line',self.x,self.y,10)
   end
 end
 
